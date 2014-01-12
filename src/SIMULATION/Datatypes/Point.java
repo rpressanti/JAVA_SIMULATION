a38de935a62f2db79a3df3be5bfd3cc9c6187bc5
Richard Pressanti <diamaths@mail.com>
2014-01-12 13:18:13 +0100

    Implementation de la classe Point

--- src/SIMULATION/Datatypes/Point.java
+++ src/SIMULATION/Datatypes/Point.java
@@ -1,5 +1,59 @@
 package SIMULATION.Datatypes;
 
+
+
 public class Point {
 
+	protected double x ;
+	protected double y ;
+	
+	
+	
+	
+	Point( ) {
+		this.x = this.y = 0 ;
+	}
+	
+	
+	
+	Point( double x , double y ) {
+		this.x = x ;
+		this.y = y ;
+	}
+
+	
+	
+	
+	public boolean update( double x , double y) {
+		
+		boolean modifie = false ;
+		
+		if( this.x != x ) {
+			this.x = x ;
+			modifie = true ;
+		}
+
+		if( this.y != y ) {
+			this.y = y ;
+			modifie = true ;
+		}
+		
+		return modifie ;
+	}
+	
+	public double getX() {
+		return this.x ;
+	}
+	
+	public double getY() {
+		return this.y ;
+	}
+	
+	
+	public double distanceTo( Point p ) {
+		return  Math.sqrt( 
+					Math.pow( this.getX() - p.getX() , 2 ) 
+				+	Math.pow( this.getY() - p.getY() , 2 )
+				)  ;
+	}
 }
